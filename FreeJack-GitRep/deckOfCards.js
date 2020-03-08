8bfc68079d0752d3b7b499a7c427511318ce5566
akrbanjevic1 <akrbanjevic1@luc.edu>
2020-03-08 11:22:24 -0500

    Changed deckOfCards file. Created all the necessary functions first

---
+++ FreeJack-GitRep/deckOfCards.js
@@ -0,0 +1,62 @@
+const numberOfCards = 0;
+//Here, we instantiate the 2 objects used for the game, player and dealer.
+let player = {
+    cards: [],
+    money: 50,
+    currentScore: 0
+};
+
+let dealer = {
+    cards: [],
+    currentScore: 0
+};
+
+//Here, we are getting the HTML elements (byID). elements needed: hit, stand, (anything else?)
+
+//Here, we want to instantiate the deck. 
+let deck = {
+    
+}
+//Here, we create the necessary game functions.
+
+
+function valueOfCards(cards) {
+    let cardSum = 0;
+
+    for (i = 0; i < cards.length; i += 1) {
+        if (cards[i])
+    }
+}
+
+function bet() {
+
+}
+
+function drawDealer() {
+
+}
+
+function endGame() {
+
+}
+
+function newGame() {
+
+}
+
+function hit() {
+
+}
+
+function stand() {
+
+}
+
+function reset() {
+
+}
+
+
+
+
+
