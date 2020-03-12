9f75e4eeca5544f43ea1ea182e78f2061c49f12c
akrbanjevic1 <akrbanjevic1@luc.edu>
2020-03-11 19:44:32 -0500

    Added msgbox to HTML. Added the crucial endgame function (which has the purpose of checking dealer and player card values. Created separate functions for getting the deck array and setting it. Added preliminary hit function to push card from the main deck into the players cards array. Added preliminary newGame function (have not tested though)

--- FreeJack-GitRep/deckOfCards.js
+++ FreeJack-GitRep/deckOfCards.js
@@ -1,4 +1,3 @@
-const numberOfCards = 0;
 //Here, we instantiate the 2 objects used for the game, player and dealer.
 let player = {
     cards: [],
@@ -10,14 +9,14 @@ let dealer = {
     cards: [],
     currentScore: 0
 };
-
+const numberOfCards = 0; //using this to keep number of cards pulled!
 //Here, we are getting the HTML elements (byID). elements needed: hit, stand, (anything else?)
 
 //Here, we want to instantiate the deck. 
 let deck = {
     deckArr = [],
 
-    createDeck: function() {
+    fillDeck: function() {
         let ranks;
         let suits;
         ranks = [2,3,4,5,6,7,8,9,10,"K","Q","J","A"];
@@ -34,19 +33,30 @@ let deck = {
         }
     },
 
-    shuffleDeck: function() {
+    getDeckArr: function() {
+        return this.deckArr;
+    },
+
+    setDeckArr: function(deckArr2) {
+        this.deckArr = deckArr2;
+    }
+}
+
+function shuffleDeck(deck) {
         let temporaryVal;
-        let deckSize = deckArr.length;
+        let tempDeckArr = deck.getDeckArr();
+        let deckSize = tempDeckArr.length();
         let randomVal;
 
         for(let i = 0; i < deckSize; i++) {
-            temporaryVal = deckArr[i];
-            randomVal = Math.floor(Math.random() * (deckArr.length+1 - (0 + 1)) + 0);
-            deckArr[i] = deckArr[randomVal];
-            deckArr[randomVal] = temporaryVal; 
+            temporaryVal = tempDeckArr[i];
+            randomVal = Math.floor(Math.random() * (tempDeckArr.length+1 - (0 + 1)) + 0);
+            tempDeckArr[i] = tempDeckArr[randomVal];
+            tempDeckArr[randomVal] = temporaryVal; 
         }
-    }
+        deck.setDeckArr(tempDeckArr);
 }
+
 //Here, we create the necessary game functions.
 
 function valueOfCards(cards) {
@@ -78,32 +88,80 @@ while (numAces >= 1) {
   }
 }
 
-function bet() {
+function makeBet() {
 
 }
 
-function drawDealer() {
-
-}
-
-function endGame() {
-
-}
-
-function newGame() {
+function draw() {
 
 }
 
 function hit() {
+    //Grabbing a card from the deck, and then placing it in the players cards array.
+    let tempCard = deck.deckArr[numberOfCards];
+    player.cards.push(tempCard);
+    //Finding the sum of the card values currently in hand. 
+    let tempScore = valueOfCards(player.cards);
+    let playerCardsString = JSON.stringify(player.cards);
+    //Here we edit the HTML after the player has asked to hit.
+    document.getElementById("Score: Player").innerHTML = "Score Player: " + tempScore;
+    document.getElementById("Player Cards").innerHTML = "Player's Current Cards: " + playerCardsString;
+    numberOfCards++;
+
 
 }
 
 function stand() {
-
+    let score = dealer.currentScore;
+    while (score < 17) {
+        draw();
+    }
 }
 
-function reset() {
+function newGame() {
+    document.getElementById("btnStart").disabled = true;
+    document.getElementById("btnHit").disabled = false;
+    document.getElementById("btnStay").disabled = false;
 
+    hit();
+    hit();
+    draw();
+    endGame();
+}
+
+function endGame() {
+    let tempPlayerScore = player.score;
+    let tempPlayerMoney = player.money;
+    let tempDealerScore = dealer.score;
+
+    if(tempPlayerScore === 21) {
+        //Player wins!
+        document.getElementById("msgBox").innerHTML = "Congratulations, you win!";
+        document.getElementById("Money").innerHTML = "Total Money: " + tempPlayerMoney;
+        //need to reset board here somehow...
+    }
+
+    if(tempDealerScore === 21) {
+        document.getElementById("msgBox").innerHTML = "Uh oh, looks like the dealer wins :(";
+        //reset the board...
+    }
+
+    if(tempPlayerScore > 21) {
+        document.getElementById("msgBox").innerHTML = "Sorry, your total card value is over 21. Dealer wins :(";
+        //reset the board...
+    }
+
+    if(tempDealerScore > 21) {
+        document.getElementById("msgBox").innerHTML = "Congratulations, dealer score is greater than 21. You win!";
+        //reset the board...
+    }
+
+    if (tempPlayerMoney === 0) {
+        document.getElementById("msgBox").innerHTML = "Sorry, you ran out of money! :((";
+        //disable the buttons to play
+        document.getElementById("btnStay").disabled = true;
+        document.getElementById("btnHit").disabled = true;
+    }
 }
 
 
