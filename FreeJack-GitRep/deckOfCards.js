70e0a2f00a3e6b89eb443db1aedeb7e1a1083a01
akrbanjevic1 <akrbanjevic1@luc.edu>
2020-04-13 12:30:17 -0500

    Added canvas element to HTML. Created JS function to display dealer cards on canvas. Still need to fix though...

--- FreeJack-GitRep/deckOfCards.js
+++ FreeJack-GitRep/deckOfCards.js
@@ -109,6 +109,11 @@ function drawDealer() {
     let tempIndex = numberOfCards;
     let tempCards = deck.deckArr[tempIndex];
     dealer.cards.push(tempCards);
+    /* let testCard = {
+        suit: "clubs",
+        rank: 2
+    };
+    dealer.cards.push(testCard); */
     let tempScore = valueOfCards(dealer.cards);
     dealer.currentScore = tempScore; //Here, we temporarily need the current score of the dealer. Then we grab their cards as
     //String (using json.stringify)
@@ -116,8 +121,237 @@ function drawDealer() {
     document.getElementById("Dealer Cards").innerHTML = "Dealer's Cards: " + dealerCardsString;
     document.getElementById("Score: Dealer").innerHTML = "Score Dealer: " + tempScore;
 
+    for(let i = 0; i < dealer.cards.length; i++) {
+        drawCardImage(dealer.cards[i],i);
+    } 
     endGame();
     numberOfCards++;
+}
+
+function drawCardImage(card, i) {
+    var dealerCanvas = document.getElementById('dealerCanvas');
+    var context = dealerCanvas.getContext('2d');
+    let currentCard = new Image();
+        //Here we want the card images to display.
+        if (context) {
+            //for(let i = 0; i < tempCards.length; i++) {
+                currentCard.onload = function() {
+                    context.drawImage(currentCard, (i+84), 0,80,80);
+                }
+
+                if(card.suit === "clubs" && card.rank === 10) {
+                    currentCard.src = 'https://i.postimg.cc/zDPtgj9L/10C.jpg';
+                }
+
+                if(card.suit === "diamonds" && card.rank === 10) {
+                    currentCard.src = 'https://i.postimg.cc/cHFDTMCb/10D.jpg';
+                }
+
+                if(card.suit === "hearts" && card.rank === 10) {
+                    currentCard.src = 'https://i.postimg.cc/tJ7BcZVC/10H.jpg';
+                }
+
+                if(card.suit === "spades" && card.rank === 10) {
+                    currentCard.src = 'https://i.postimg.cc/6QLb4NXw/10S.jpg';
+                }
+
+                if(card.suit === "clubs" && card.rank === 2) {
+                    currentCard.src = 'https://i.postimg.cc/j2KsQTWX/2C.jpg';
+                }
+
+                if(card.suit === "diamonds" && card.rank === 2) {
+                    currentCard.src = 'https://i.postimg.cc/zXxJW9qC/2D.jpg';
+                }
+
+                if(card.suit === "hearts" && card.rank === 2) {
+                    currentCard.src = 'https://i.postimg.cc/nhMn10L6/2H.jpg';
+                }
+
+                if(card.suit === "clubs" && card.rank === 2) {
+                    currentCard.src = 'https://i.postimg.cc/Kcrcc1Vs/2S.jpg';
+                }
+
+                if(card.suit === "clubs" && card.rank === 3) {
+                    currentCard.src = 'https://i.postimg.cc/63VKQPHG/3C.jpg';
+                }
+
+                if(card.suit === "diamonds" && card.rank === 3) {
+                    currentCard.src = 'https://i.postimg.cc/jj7Kp2Pc/3D.jpg';
+                }
+
+                if(card.suit === "hearts" && card.rank === 3) {
+                    currentCard.src = 'https://i.postimg.cc/C1pYS04g/3H.jpg';
+                }
+
+                if(card.suit === "spades" && card.rank === 3) {
+                    currentCard.src = 'https://i.postimg.cc/jdxxDsYq/3S.jpg';
+                }
+
+                if(card.suit === "clubs" && card.rank === 4) {
+                    currentCard.src = 'https://i.postimg.cc/7hmy32rH/4C.jpg';
+                }
+
+                if(card.suit === "diamonds" && card.rank === 4) {
+                    currentCard.src = 'https://i.postimg.cc/9XpV5vR6/4D.jpg';
+                }
+
+                if(card.suit === "hearts" && card.rank === 4) {
+                    currentCard.src = 'https://i.postimg.cc/02XsrDCZ/4H.jpg';
+                }
+
+                
+                if(card.suit === "spades" && card.rank === 4) {
+                    currentCard.src = 'https://i.postimg.cc/fTRswBYJ/4S.jpg';
+                }
+
+                if(card.suit === "clubs" && card.rank === 5) {
+                    currentCard.src = 'https://i.postimg.cc/sgtC4zqh/5C.jpg';
+                }
+
+                if(card.suit === "diamonds" && card.rank === 5) {
+                    currentCard.src = 'https://i.postimg.cc/tTb085XR/5D.jpg';
+                }
+
+                if(card.suit === "hearts" && card.rank === 5) {
+                    currentCard.src = 'https://i.postimg.cc/9fZ5xSHN/5H.jpg';
+                }
+
+                if(card.suit === "spades" && card.rank === 5) {
+                    currentCard.src = 'https://i.postimg.cc/zGL1hvtN/5S.jpg';
+                }
+
+                if(card.suit === "clubs" && card.rank === 6) {
+                    currentCard.src = 'https://i.postimg.cc/GtMZ8nv9/6C.jpg';
+                }
+
+                if(card.suit === "diamonds" && card.rank === 6) {
+                    currentCard.src = 'https://i.postimg.cc/bJRXPtJx/6D.jpg';
+                }
+
+                if(card.suit === "hearts" && card.rank === 6) {
+                    currentCard.src = 'https://i.postimg.cc/nV08wBNZ/6H.jpg';
+                }
+
+                if(card.suit === "spades" && card.rank === 6) {
+                    currentCard.src = 'https://i.postimg.cc/vT8JZZgp/6S.jpg';
+                }
+
+                if(card.suit === "clubs" && card.rank === 7) {
+                    currentCard.src = 'https://i.postimg.cc/xT4w9Y04/7C.jpg';
+                }
+
+                if(card.suit === "diamonds" && card.rank === 7) {
+                    currentCard.src = 'https://i.postimg.cc/t4nLTfJC/7D.jpg';
+                }
+
+                if(card.suit === "hearts" && card.rank === 7) {
+                    currentCard.src = 'https://i.postimg.cc/MGkgjDf0/7H.jpg';
+                }
+
+                if(card.suit === "spades" && card.rank === 7) {
+                    currentCard.src = 'https://i.postimg.cc/QxCLLVr7/7S.jpg';
+                }
+
+                if(card.suit === "clubs" && card.rank === 8) {
+                    currentCard.src = 'https://i.postimg.cc/2y5tf9gY/8C.jpg';
+                }
+
+                
+                if(card.suit === "diamonds" && card.rank === 8) {
+                    currentCard.src = 'https://i.postimg.cc/85N3ZzHj/8D.jpg';
+                }
+
+                
+                if(card.suit === "hearts" && card.rank === 8) {
+                    currentCard.src = 'https://i.postimg.cc/C172XpPJ/8H.jpg';
+                }
+
+                
+                if(card.suit === "spades" && card.rank === 8) {
+                    currentCard.src = 'https://i.postimg.cc/8Ch9d6L2/8S.jpg';
+                }
+
+                if(card.suit === "clubs" && card.rank === 9) {
+                    currentCard.src = 'https://i.postimg.cc/P5DRhwVC/9C.jpg';
+                }
+
+                if(card.suit === "diamonds" && card.rank === 9) {
+                    currentCard.src = 'https://i.postimg.cc/dtLfHX8m/9D.jpg';
+                }
+
+                if(card.suit === "hearts" && card.rank === 9) {
+                    currentCard.src = 'https://i.postimg.cc/VLpV3Rgy/9H.jpg';
+                }
+
+                if(card.suit === "spades" && card.rank === 9) {
+                    currentCard.src = 'https://i.postimg.cc/j2kFqQr7/9S.jpg';
+                }
+
+                if(card.suit === "diamonds" && card.rank === "A") {
+                    currentCard.src = 'https://i.postimg.cc/FsdPgS4T/AD.jpg';
+                }
+
+                if(card.suit === "hearts" && card.rank === "A") {
+                    currentCard.src = 'https://i.postimg.cc/VLxK7Nsm/AH.jpg';
+                }
+
+                if(card.suit === "spades" && card.rank === "A") {
+                    currentCard.src = 'https://i.postimg.cc/wjD0nd3J/AS.jpg';
+                }
+
+                if(card.suit === "clubs" && card.rank === "A") {
+                    currentCard.src = 'https://i.postimg.cc/pr7sMWDQ/AC.jpg';
+                }
+
+                if(card.suit === "clubs" && card.rank === "J") {
+                    currentCard.src = 'https://i.postimg.cc/TYD9WXVS/JC.jpg';
+                }
+
+                if(card.suit === "diamonds" && card.rank === "J") {
+                    currentCard.src = 'https://i.postimg.cc/jSYhHkvj/JD.jpg';
+                }
+
+                if(card.suit === "hearts" && card.rank === "J") {
+                    currentCard.src = 'https://i.postimg.cc/zGwF0SPF/JH.jpg';
+                }
+
+                if(card.suit === "spades" && card.rank === "J") {
+                    currentCard.src = 'https://i.postimg.cc/wvrX7W7g/JS.jpg';
+                }
+
+                if(card.suit === "clubs" && card.rank === "K") {
+                    currentCard.src = 'https://i.postimg.cc/mhf7StYn/KC.jpg';
+                }
+
+                if(card.suit === "diamonds" && card.rank === "K") {
+                    currentCard.src = 'https://i.postimg.cc/0563ZFGG/KD.jpg';
+                }
+
+                if(card.suit === "hearts" && card.rank === "K") {
+                    currentCard.src = 'https://i.postimg.cc/HsXN1MhV/KH.jpg';
+                }
+
+                if(card.suit === "spades" && card.rank === "K") {
+                    currentCard.src = 'https://i.postimg.cc/76jKpP13/KS.jpg';
+                }
+
+                if(card.suit === "clubs" && card.rank === "Q") {
+                    currentCard.src = 'https://i.postimg.cc/hvx2gFMH/QC.jpg';
+                }
+
+                if(card.suit === "diamonds" && card.rank === "Q") {
+                    currentCard.src = 'https://i.postimg.cc/y6SQ09Bv/QD.jpg';
+                }
+
+                if(card.suit === "hearts" && card.rank === "Q") {
+                    currentCard.src = 'https://i.postimg.cc/rFXhjmwc/QH.jpg';
+                }
+
+                if(card.suit === "spades" && card.rank === "Q") {
+                    currentCard.src = 'https://i.postimg.cc/pXfs6Hmj/QS.jpg';
+                }
+                //}
+        }
 }
 
 function hit() {
@@ -135,7 +369,7 @@ function hit() {
     numberOfCards++;
 
     endGame();
-   
+    //document.getElementById('btnStart').disabled = false;
 }
 
 function stand() {
@@ -147,20 +381,19 @@ function stand() {
         tempDealerScore = dealer.currentScore;
     }
     endGame();
-    
+    //document.getElementById('btnStart').disabled = false;
 }
 
 function newGame() {
     deck.fillDeck();
-    //deck.shuffleDeck();
     shuffletheDeck(); //New method made to reduce complexity of code in the object named deck
     document.getElementById("btnStart").disabled = true;
     document.getElementById("btnHit").disabled = false;
     document.getElementById("btnStay").disabled = false;
-    
     hit();
     hit();
     drawDealer();
+
     document.getElementById("Money").innerHTML = player.money;
     endGame();
 }
@@ -179,6 +412,7 @@ function endGame() {
         document.getElementById("btnHit").disabled = true;
         document.getElementById("btnStay").disabled = true;
         document.getElementById("btnStart").disabled = false;
+
     }
 
     if(tempDealerScore === 21) {
@@ -188,6 +422,7 @@ function endGame() {
         document.getElementById("btnStay").disabled = true;
         document.getElementById('Money').innerHTML = "Total Money: " + tempPlayerMoney;
         document.getElementById("btnStart").disabled = false;
+
     }
 
     if(tempPlayerScore > 21) {
@@ -197,6 +432,7 @@ function endGame() {
         document.getElementById("btnStay").disabled = true;
         document.getElementById('Money').innerHTML = "Total Money: " + tempPlayerMoney;
         document.getElementById("btnStart").disabled = false;
+    
     }
 
     if(tempDealerScore > 21) {
@@ -206,8 +442,8 @@ function endGame() {
         document.getElementById("btnStay").disabled = true;
         document.getElementById('Money').innerHTML = "Total Money: " + tempPlayerMoney;
         document.getElementById("btnStart").disabled = false;
-    }
 
+    }
     if (tempPlayerMoney === 0) {
         document.getElementById('msgBox').innerHTML = "Sorry, you ran out of money! :((";
         //disable the buttons to play
@@ -226,6 +462,7 @@ function endGame() {
         document.getElementById("btnHit").disabled = true;
         document.getElementById('Money').innerHTML = "Total Money: " + tempPlayerMoney;
         document.getElementById("btnStart").disabled = false;
+
     }
 
     if (tempDealerScore >= 17 && (tempPlayerScore > tempDealerScore) && tempPlayerScore < 21) {
@@ -235,6 +472,7 @@ function endGame() {
         document.getElementById("btnHit").disabled = true;
         document.getElementById('Money').innerHTML = "Total Money: " + tempPlayerMoney;
         document.getElementById("btnStart").disabled = false;
+        
     }
 
     if (tempDealerScore >= 17 && (tempPlayerScore === tempDealerScore) && tempDealerScore < 21) {
@@ -243,6 +481,12 @@ function endGame() {
         document.getElementById("btnHit").disabled = true;
         document.getElementById('Money').innerHTML = "Total Money: " + tempPlayerMoney;
         document.getElementById("btnStart").disabled = false;
+        
     }
     
+}
+
+function resetCardString() {
+    document.getElementById("Dealer Cards").innerHTML = "";
+    document.getElementById("Player Cards").innerHTML = "";
 }
